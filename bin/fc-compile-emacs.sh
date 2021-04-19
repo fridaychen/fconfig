72ffdc33bf03734cd4d4467e71ab2f38c2796287
Ka Chin <zg-ka.chin@phoenixeng.co.jp>
2021-04-19 14:50:34 +0900

    use emacs mirror at github

--- bin/fc-compile-emacs.sh
+++ bin/fc-compile-emacs.sh
@@ -3,7 +3,7 @@
 sudo apt build-dep emacs
 sudo apt install libharfbuzz-dev
 
-git clone git://git.savannah.gnu.org/emacs.git -b feature/native-comp
+git clone git://github.com/emacs-mirror/emacs.git -b feature/native-comp
 cd emacs
 ./autogen.sh
 ./configure --with-native-compilation

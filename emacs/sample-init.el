78f1cd6bf9d83d63c24336e51f63557c40268091
Ka Chin <zg-ka.chin@phoenixeng.co.jp>
2021-04-26 17:52:07 +0900

    use separate custom file

--- emacs/sample-init.el
+++ emacs/sample-init.el
@@ -2,22 +2,9 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-                                        ;(package-initialize)
+;;(package-initialize)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(sml/col-number ((t (:inherit sml/global :weight normal))))
- '(sml/line-number ((t (:inherit sml/global :weight normal)))))
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
 (defun fc-user-config ()
   (setf *fc-font-mode-line-delta* 0)

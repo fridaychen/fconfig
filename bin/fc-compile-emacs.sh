fd6c3822b782f69d46f2c485247ad035e25af349
Ka Chin <zg-ka.chin@phoenixeng.co.jp>
2021-05-17 19:15:39 +0900

    add parameter

--- bin/fc-compile-emacs.sh
+++ bin/fc-compile-emacs.sh
@@ -3,8 +3,8 @@
 sudo apt build-dep emacs
 sudo apt install libharfbuzz-dev
 
-git clone git://github.com/emacs-mirror/emacs.git -b feature/native-comp
+git clone git://github.com/emacs-mirror/emacs.git
 cd emacs
 ./autogen.sh
-./configure --with-native-compilation
+./configure --with-native-compilation --with-json
 make -j$(nproc)

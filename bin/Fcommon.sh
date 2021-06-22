5968b3df6a782c68f87961699c57a465da8cfbc5
Ka Chin <zg-ka.chin@phoenixeng.co.jp>
2021-06-22 18:30:40 +0900

    refactor

--- bin/Fcommon.sh
+++ bin/Fcommon.sh
@@ -33,7 +33,7 @@ function fc-net-connected() {
 }
 
 function fc-user-confirm() {
-    # local opt
+    local opt
     while read -e -t 0.1; do :; done
     read -n 1 -s -r -p "$* [y/N] ? " opt
     echo -e "\n"

0bf2a931a0864b166d67f7f24fc7d4ebcf8ed2fe
friday chen <fridaychen@gmail.com>
2021-07-07 19:57:37 +0900

    add bc

--- bin/Fsetup-app.sh
+++ bin/Fsetup-app.sh
@@ -2,7 +2,7 @@
 
 case $(uname) in
     Darwin)
-        brew install bear cmake htop iterm2 make most mpg123 pandoc sqlite tmux the_silver_searcher tidy-html5
+        brew install bc bear cmake htop iterm2 make most mpg123 pandoc sqlite tmux the_silver_searcher tidy-html5
 
         brew install --cask adobe-acrobat-reader amazon-music calibre goldendict kitty libreoffice microsoft-excel microsoft-powerpoint microsoft-word mpv openemu quodlibet
         ;;
@@ -12,12 +12,12 @@ case $(uname) in
 
         case $ID in
             debian)
-                sudo apt install build-essential cowsay bear cmake espeak-ng fasd figlet ffmpeg fortunes graphviz htop lolcat mbrola mbrola-us2 meld most mpg123 mpv neovim pandoc python3-pip ripgrep scons silversearcher-ag tidy
+                sudo apt install bc build-essential cowsay bear cmake espeak-ng fasd figlet ffmpeg fortunes graphviz htop lolcat mbrola mbrola-us2 meld most mpg123 mpv neovim pandoc python3-pip ripgrep scons silversearcher-ag tidy
                 ;;
 
             ubuntu)
                 sudo add-apt-repository ppa:mmstick76/alacritty
-                sudo apt install alacritty bat build-essential cowsay bear cmake espeak-ng fasd figlet ffmpeg graphviz htop lolcat mbrola mbrola-us2 meld most mpg123 mpv neovim pandoc ripgrep scons silversearcher-ag tidy
+                sudo apt install alacritty bat bc build-essential cowsay bear cmake espeak-ng fasd figlet ffmpeg graphviz htop lolcat mbrola mbrola-us2 meld most mpg123 mpv neovim pandoc ripgrep scons silversearcher-ag tidy
 
                 for x in lsd shfmt; do
                     sudo snap install $x
@@ -34,7 +34,7 @@ case $(uname) in
                 ;;
 
             arch | manjaro*)
-                sudo pacman -S --needed alacritty avahi base-devel bat cowsay cmake espeak-ng fasd figlet ffmpeg go graphviz htop lolcat meld most mpg123 mpv neovim nss-mdns p7zip scons shfmt ripgrep tidy
+                sudo pacman -S --needed alacritty avahi base-devel bat bc cowsay cmake espeak-ng fasd figlet ffmpeg go graphviz htop lolcat meld most mpg123 mpv neovim nss-mdns p7zip scons shfmt ripgrep tidy
                 ;;
         esac
         ;;

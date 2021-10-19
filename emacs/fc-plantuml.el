712557c17509f1bc1933ac1471deaf8608523cbb
friday chen <fridaychen@gmail.com>
2021-10-19 23:59:13 +0900

    add

--- emacs/fc-plantuml.el
+++ emacs/fc-plantuml.el
@@ -8,7 +8,7 @@
   (fc-make-keymap
    `(
      ("c" plantuml-preview)
-     ("U" plantuml-download-jar)
+     ("D" plantuml-download-jar)
      )
    "fc-plant-uml-map"
    *fc-func-mode-map*)
@@ -19,23 +19,30 @@
 
 (fc-load 'plantuml-mode
   :after (progn
-           (let ((jar (format "%s/site/resource/plantuml.jar" *fc-home*)))
-             (setf plantuml-jar-path jar
-                   plantuml-default-exec-mode 'jar
-                   org-plantuml-jar-path jar))
+	   (let ((jar (format "%s/site/resource/plantuml.jar" *fc-home*)))
+	     (setf plantuml-jar-path jar
+		   plantuml-default-exec-mode 'jar
+		   org-plantuml-jar-path jar))
 
-           (unless (file-exists-p plantuml-jar-path)
-             (plantuml-download-jar))
+	   (unless (file-exists-p plantuml-jar-path)
+	     (plantuml-download-jar))
 
-           (add-to-list 'auto-mode-alist
-                        '("\\.plantuml\\'" . plantuml-mode))
+	   (add-to-list 'auto-mode-alist
+			'("\\.plantuml\\'" . plantuml-mode))
 
-           (with-eval-after-load 'org
-             (require 'ob-plantuml))))
+	   (with-eval-after-load 'org
+	     (require 'ob-plantuml))))
 
 (fc-load 'flycheck-plantuml
   :after (progn
-           (flycheck-plantuml-setup)))
+	   (flycheck-plantuml-setup)))
+
+(cl-defun fc--insert-note ()
+  "return note command."
+  (let* ((pos (fc-user-select "Note type" '("left" "over" "right")))
+	 (arg (when (string= pos "over")
+		(concat " " (read-string "Actor/Participant")))))
+    (concat pos arg)))
 
 (provide 'fc-plantuml)
 

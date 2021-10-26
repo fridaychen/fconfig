57445ef59c969e296818a5b27e378c6b47040752
Ka Chin <zg-ka.chin@phoenixeng.co.jp>
2021-10-26 15:07:46 +0900

    bugfix

--- emacs/fc-next-error.el
+++ emacs/fc-next-error.el
@@ -9,14 +9,24 @@
                                    ggtags-navigation-mode
                                    grep-mode
                                    xref--xref-buffer-mode))
-
 (fc-load 'simple
   :local t
   :after (progn
+           (defun fc--next-error-buffer-p (buf)
+             (member (buffer-local-value 'major-mode buf)
+                     *fc-navi-buffer-modes*))
+
            (defun fc--find-visible-next-error-buffer ()
              "Find visible next-error buffer by major-mode."
-             (cdr (fc-first-window (with-current-buffer (cdr it)
-                                     (member major-mode *fc-navi-buffer-modes*)))))
+             (let ((buf (cdr (fc-first-window
+                              (fc--next-error-buffer-p (cdr it))))))
+               (if (and buf
+                        (eq (buffer-local-value 'major-mode buf)
+                            'flycheck-error-list-mode))
+                   (if next-error-function
+                       (current-buffer)
+                     nil)
+                 buf)))
 
            (defun fc--next-error-find-buffer (&rest _args)
              (or (fc--find-visible-next-error-buffer)
@@ -35,7 +45,7 @@
                               (lambda ()
                                 (member major-mode *fc-navi-buffer-modes*)))
               :error-msg "No navigatable buffer found."
-              :pop t))
+              :pop (not (fc--next-error-buffer-p (current-buffer)))))
 
            (setf next-error-find-buffer-function #'fc--next-error-find-buffer)))
 

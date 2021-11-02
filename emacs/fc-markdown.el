9ebf8434aeca42fdc667125350fc9f2716de8a83
friday chen <fridaychen@gmail.com>
2021-11-02 22:44:14 +0900

    bugfix

--- emacs/fc-markdown.el
+++ emacs/fc-markdown.el
@@ -15,8 +15,7 @@
           '("---"))
 
     (defun fc--setup-markdown-mode ()
-      (when (string= (fc--md-lang) "en-US")
-        (visual-line-mode 1))
+      (visual-line-mode (if (string= (fc--md-lang) "en-US") 1 -1))
 
       (let ((buf (current-buffer)))
         (fc-delay-task #'(lambda ()

2b3759e4a5c0259445e55653a263aac73ec516b9
friday chen <fridaychen@gmail.com>
2021-11-12 08:12:35 +0900

    adjust soonthe percent

--- emacs/fc-common.el
+++ emacs/fc-common.el
@@ -154,7 +154,7 @@ COLOR: background color."
       (fc-set-face-attribute it nil :background new-bg))))
 
 (defvar *fc-soothe-dark-percent* -4)
-(defvar *fc-soothe-light-percent* 5)
+(defvar *fc-soothe-light-percent* 10)
 
 (defconst *fc-soothe-color* (make-hash-table))
 (--each '((material "gray20")

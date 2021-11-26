8a151ee5f5f48ff16201ca76fca01fb63b0891e5
friday chen <fridaychen@gmail.com>
2021-11-26 20:55:24 +0900

    typo

--- emacs/fc-plantuml.el
+++ emacs/fc-plantuml.el
@@ -12,36 +12,36 @@
      )
    "fc-plant-uml-map"
    *fc-func-mode-map*)
-  "KEYS c: preview  E: org edit exit  U: update jar.")
+  "KEYS c: preview  D: update jar  E: org edit exit.")
 
 (cl-defun fc-plantuml-mode-func ()
   (fc-modal-head-key "Plant-uml" '*fc-plant-uml-map*))
 
 (fc-load 'plantuml-mode
   :after (progn
-	   (let ((jar (format "%s/site/resource/plantuml.jar" *fc-home*)))
-	     (setf plantuml-jar-path jar
-		   plantuml-default-exec-mode 'jar
-		   org-plantuml-jar-path jar))
+           (let ((jar (format "%s/site/resource/plantuml.jar" *fc-home*)))
+             (setf plantuml-jar-path jar
+                   plantuml-default-exec-mode 'jar
+                   org-plantuml-jar-path jar))
 
-	   (unless (file-exists-p plantuml-jar-path)
-	     (plantuml-download-jar))
+           (unless (file-exists-p plantuml-jar-path)
+             (plantuml-download-jar))
 
-	   (add-to-list 'auto-mode-alist
-			'("\\.plantuml\\'" . plantuml-mode))
+           (add-to-list 'auto-mode-alist
+                        '("\\.plantuml\\'" . plantuml-mode))
 
-	   (with-eval-after-load 'org
-	     (require 'ob-plantuml))))
+           (with-eval-after-load 'org
+             (require 'ob-plantuml))))
 
 (fc-load 'flycheck-plantuml
   :after (progn
-	   (flycheck-plantuml-setup)))
+           (flycheck-plantuml-setup)))
 
 (cl-defun fc--insert-note ()
   "return note command."
   (let* ((pos (fc-user-select "Note type" '("left" "over" "right")))
-	 (arg (when (string= pos "over")
-		(concat " " (read-string "Actor/Participant")))))
+         (arg (when (string= pos "over")
+                (concat " " (read-string "Actor/Participant")))))
     (concat pos arg)))
 
 (provide 'fc-plantuml)

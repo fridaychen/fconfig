0778b7417ba5c628bc56f703ad05da30c3a1d4b8
friday chen <fridaychen@gmail.com>
2021-12-20 22:38:40 +0900

    bugfix for emacs28

--- emacs/fc-modeline.el
+++ emacs/fc-modeline.el
@@ -22,11 +22,11 @@
 (defvar *fc-selected-window* (frame-selected-window))
 
 (defvar *fc-buffer-id-keymap* (fc-make-keymap
-                               '(([mode-line mouse-1] ivy-switch-buffer))
-                               "fc-buffer-id-keymap"))
+			       '(([mode-line mouse-1] ivy-switch-buffer))
+			       "fc-buffer-id-keymap"))
 (defvar *fc-pos-keymap* (fc-make-keymap
-                         '(([mode-line mouse-1] counsel-imenu))
-                         "fc-pos-keymap"))
+			 '(([mode-line mouse-1] counsel-imenu))
+			 "fc-pos-keymap"))
 
 (defun fc--active-window-p ()
   "Test if current window is active."
@@ -66,19 +66,19 @@
   "Test if current window is at right-bottom."
   (or (one-window-p)
       (and (not (window-right (get-buffer-window)))
-           (not (window-right (window-parent)))
-           (not (window-next-sibling)))))
+	   (not (window-right (window-parent)))
+	   (not (window-next-sibling)))))
 
 (defvar-local *fc-enable-state-seg* t)
 
 (cl-defun fc--state-seg ()
   "Buffer state segment."
   (unless (and *fc-enable-state-seg*
-               (not (fc--extreme-narrow-window-p)))
+	       (not (fc--extreme-narrow-window-p)))
     (cl-return-from fc--state-seg))
 
   (let* ((state (concat (if buffer-read-only "%%" "-")
-                        (if (buffer-modified-p)  "*" "-"))))
+			(if (buffer-modified-p)  "*" "-"))))
     (when (fboundp 'fc-modeline-extra-state)
       (setf state (fc-modeline-extra-state state)))
 
@@ -89,7 +89,7 @@
 (defun fc--major-mode-seg ()
   "The name of the major mode."
   (when (and *fc-enable-major-mode-seg*
-             (fc--wide-window-p))
+	     (fc--wide-window-p))
     mode-name))
 
 (defvar-local *fc-buffer-title-seg* nil)
@@ -102,13 +102,13 @@
 (defun fc--buffer-full-id ()
   "Generate buffer full id."
   (let ((rhost (fc--remote-buffer-p))
-        (proj (fc-modeline-proj-name)))
+	(proj (fc-modeline-proj-name)))
     (fc-text
      (list
       (when rhost
-        (concat rhost "/"))
+	(concat rhost "/"))
       (when proj
-        (concat proj "::"))
+	(concat proj "::"))
       (buffer-name))
      :separator ""
      :keys *fc-buffer-id-keymap*)))
@@ -116,9 +116,9 @@
 (defun fc--buffer-short-id ()
   "Generate buffer short id."
   (fc-text (buffer-name)
-           :limit (ceiling
-                   (* (window-width) 0.35))
-           :keys *fc-buffer-id-keymap*))
+	   :limit (ceiling
+		   (* (window-width) 0.35))
+	   :keys *fc-buffer-id-keymap*))
 
 (defun fc--buffer-title-seg ()
   "Buffer title segment."
@@ -151,13 +151,13 @@
   "VC state segment."
   (when vc-mode
     (let ((color (pcase (vc-state buffer-file-name)
-                   ('edited "#cf6a4c")
-                   ((or 'needs-merge 'conflict) "#ff0066"))))
+		   ('edited "#cf6a4c")
+		   ((or 'needs-merge 'conflict) "#ff0066"))))
       (if color
-          (fc-text *fc--vc-mark*
-                   :face `(:foreground ,color)
-                   :tip '(fc-vc-seg-tip))
-        *fc--vc-mark*))))
+	  (fc-text *fc--vc-mark*
+		   :face `(:foreground ,color)
+		   :tip '(fc-vc-seg-tip))
+	*fc--vc-mark*))))
 
 (defun fc--line-col-seg ()
   "Line column segment."
@@ -165,19 +165,19 @@
     (cond
      ((eq 'pdfc-view-mode major-mode)
       (if (fboundp #'spaceline--pdfview-page-number)
-          (spaceline--pdfview-page-number)
-        "_"))
+	  (spaceline--pdfview-page-number)
+	"_"))
 
      ((and column-number-mode line-number-mode)
       (concat
        "%3l:%c"
        (cond
-        ((< (current-column) 10)
-         "  ")
-        ((< (current-column) 100)
-         " ")
-        (t
-         ""))))
+	((< (current-column) 10)
+	 "  ")
+	((< (current-column) 100)
+	 " ")
+	(t
+	 ""))))
 
      (column-number-mode
       "_:%03c")
@@ -192,15 +192,15 @@
   "Position seg."
   (when (> (buffer-size) 10240)
     (list -3
-          (fc-text
-           "%p"
-           :face (fc--modeline-get-hi-face)
-           :keys *fc-pos-keymap*))))
+	  (fc-text
+	   "%p"
+	   :face (fc--modeline-get-hi-face)
+	   :keys *fc-pos-keymap*))))
 
 (defun fc--modeline-format-left ()
   "Format left modeline."
   (let ((hi-face (fc--modeline-get-hi-face))
-        (hl-sep (fc--modeline-get-hi-sep)))
+	(hl-sep (fc--modeline-get-hi-sep)))
     (list
      hl-sep
      (fc-text
@@ -234,30 +234,30 @@
 (defun fc--modeline-format-main ()
   "Format modeline."
   (let* ((left (fc--modeline-format-left))
-         (center (fc--modeline-format-center))
-         (right (fc--modeline-format-right))
-         (most-right-str (cons '(t (:eval (fc-ml-right-sep)))
-                               *fc-modeline-most-right-string*))
-         (most-right (if (fc--right-bottom-window-p)
-                         most-right-str
-                       nil))
-         (right-len (+
-                     (string-width (format-mode-line right))
-                     (string-width (format-mode-line most-right))))
-         (padding (propertize " "
-                              'display `(space :align-to (- (+ scroll-bar scroll-bar) ,right-len)))))
+	 (center (fc--modeline-format-center))
+	 (right (fc--modeline-format-right))
+	 (most-right-str (cons '(t (:eval (fc-ml-right-sep)))
+			       *fc-modeline-most-right-string*))
+	 (most-right (if (fc--right-bottom-window-p)
+			 most-right-str
+		       nil))
+	 (right-len (+
+		     (string-width (format-mode-line right))
+		     (string-width (format-mode-line most-right))))
+	 (padding (propertize " "
+			      'display `(space :align-to (- (+ scroll-bar scroll-bar) ,right-len)))))
     (nconc left center
-           (list padding)
-           right
-           most-right)))
+	   (list padding)
+	   right
+	   most-right)))
 
 (cl-defun fc-modeline-mode ()
   "Setup mode line."
   (sit-for 1)
 
   (unless (and (facep 'default)
-               (color-defined-p (face-attribute 'default :background))
-               (color-defined-p (face-attribute 'default :foreground)))
+	       (color-defined-p (face-attribute 'default :background))
+	       (color-defined-p (face-attribute 'default :foreground)))
     (cl-return-from fc-modeline-mode))
 
   (unless (facep 'fc-modeline-highlight-face)
@@ -267,30 +267,30 @@
     (make-face 'fc-modeline-highlight-inactive-face))
 
   (let* ((deep-dark (fc-deep-dark-theme-p))
-         (bg (if deep-dark
-                 *fc-modeline-dark-active-hl-bg*
-               *fc-modeline-active-hl-bg*))
-         (fg (if deep-dark
-                 *fc-modeline-dark-active-hl-fg*
-               *fc-modeline-active-hl-fg*)))
+	 (bg (if deep-dark
+		 *fc-modeline-dark-active-hl-bg*
+	       *fc-modeline-active-hl-bg*))
+	 (fg (if deep-dark
+		 *fc-modeline-dark-active-hl-fg*
+	       *fc-modeline-active-hl-fg*)))
     (set-face-attribute 'fc-modeline-highlight-face nil
-                        :foreground fg
-                        :background bg
-                        :weight 'medium
-                        :inherit 'mode-line)
+			:foreground fg
+			:background bg
+			:weight 'medium
+			:inherit 'mode-line)
     (set-face-attribute 'fc-modeline-highlight-inactive-face nil
-                        :foreground bg
-                        :background fg
-                        :weight 'medium
-                        :inherit 'mode-line))
+			:foreground bg
+			:background fg
+			:weight 'medium
+			:inherit 'mode-line))
 
   (fc-ml-sep-reset)
 
   (fc-funcall #'mode-icons-mode :args (list -1))
   (setq-default mode-line-format
-                '("%e"
-                  (:eval
-                   (fc--modeline-format-main)))))
+		'("%e"
+		  (:eval
+		   (fc--modeline-format-main)))))
 
 (defun fc-modeline-set-selected-window (&rest _)
   "Set the variable `*fc-selected-window*' appropriately."
@@ -304,8 +304,11 @@
     (setq *fc-selected-window* nil)
     (force-mode-line-update)))
 
-(set-face-attribute 'mode-line-active nil :inherit 'mode-line)
-(set-face-attribute 'mode-line-inactive nil :inherit 'mode-line)
+(when (facep 'mode-line-active)
+  (set-face-attribute 'mode-line-active nil :inherit 'mode-line))
+
+(when (facep 'mode-line-inactive)
+  (set-face-attribute 'mode-line-inactive nil :inherit 'mode-line))
 
 (add-hook 'after-make-frame-functions #'fc-modeline-set-selected-window)
 (add-hook 'buffer-list-update-hook #'fc-modeline-set-selected-window)

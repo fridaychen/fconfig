0eb3549d2014a1351ed9eeea8a5098df2acd7698
Ka Chin <zg-ka.chin@phoenixeng.co.jp>
2022-01-30 14:58:27 +0900

    add fc-latex.sh

---
+++ bin/fc-latex.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function run-latex() {
+    export TEXINPUTS=".:..:${FCHOME}/latex//:"
+    xelatex --shell-escape ../$(basename "${1}")
+}
+
+cd $(dirname "${1}")
+
+if [[ ! -d .bin ]]; then
+    mkdir .bin
+fi
+cd .bin
+
+srcname=$(basename "$1" .tex)
+
+if [[ -f "$srcname.toc" ]]; then
+    cp "$srcname.toc" "$srcname.old.toc"
+fi
+
+run-latex "$1"
+
+if [[ ! -f "$srcname.old.toc" ]] || ! cmp -s "$srcname.toc" "$srcname.old.toc"; then
+    run-latex "$1"
+fi

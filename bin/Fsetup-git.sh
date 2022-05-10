203e3f53503aaf56bb623af237f7334836c5cc70
friday chen <fridaychen@gmail.com>
2022-05-10 11:54:32 +0900

    add git global config

--- bin/Fsetup-git.sh
+++ bin/Fsetup-git.sh
@@ -2,5 +2,12 @@
 
 git config --global user.name "friday chen"
 git config --global user.email "fridaychen@gmail.com"
+
 git config --global core.quotepath off
 git config --global core.filemode false
+
+git config --global pull.rebase true
+git config --global fetch.prune true
+git config --global diff.colorMoved zebra
+
+git config --global rebase.autoStash true

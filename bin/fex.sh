e27fd9ae7bfcd03de2fbdd90836d2b6b9cff48db
friday chen <fridaychen@gmail.com>
2022-08-17 10:59:16 +0900

    add

---
+++ bin/fex.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+function usage() {
+    echo "Usage:  $(basename $0) audio-files"
+    exit
+}
+
+function extract() {
+    local longext=${1#*.}
+    local ext=${1##*.}
+
+    case $longext in
+        tar.gz | tar.bz2 | tar)
+            tar xvf "$1"
+            return
+            ;;
+    esac
+
+    case $ext in
+        bz2)
+            bzip2 -d "$1"
+            ;;
+        gz)
+            gzip -d "$1"
+            ;;
+        rar | RAR)
+            unrar x "$1" || unar "$1"
+            ;;
+        zip | ZIP | 7z | 7Z)
+            7z e "$1"
+            ;;
+    esac
+}
+
+(($# < 1)) && usage
+
+for arg; do
+    [[ -f "$arg" ]] || {
+        echo "File $arg does not exist"
+        exit 1
+    }
+
+    extract "$arg"
+done

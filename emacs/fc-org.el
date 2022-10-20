6bb9a8985a898b386ff54e4c3c760f1d47a843a1
friday chen <fridaychen@gmail.com>
2022-10-20 15:42:50 +0900

    workaround for org publish

--- emacs/fc-org.el
+++ emacs/fc-org.el
@@ -1003,29 +1003,30 @@ LANG: language of babel."
     (message "Cannot find publish directory")
     (cl-return-from fc--org-publish))
 
-  (setq org-publish-project-alist
-        `(
-          ("org-notes"
-           :base-directory ,base-dir
-           :base-extension "org"
-           :publishing-directory ,output-dir
-           :recursive t
-           :publishing-function org-html-publish-to-html
-           :headline-levels 4
-           :auto-preamble t
-           )
+  (let ((ob-async-no-async-languages-alist '("plantuml" "python" "shell" "elisp")))
+    (setq org-publish-project-alist
+          `(
+            ("org-notes"
+             :base-directory ,base-dir
+             :base-extension "org"
+             :publishing-directory ,output-dir
+             :recursive t
+             :publishing-function org-html-publish-to-html
+             :headline-levels 4
+             :auto-preamble t
+             )
 
-          ("org-static"
-           :base-directory ,base-dir
-           :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|svg"
-           :publishing-directory ,output-dir
-           :recursive t
-           :publishing-function org-publish-attachment
-           )
+            ("org-static"
+             :base-directory ,base-dir
+             :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|svg"
+             :publishing-directory ,output-dir
+             :recursive t
+             :publishing-function org-publish-attachment
+             )
 
-          ("org" :components ("org-notes" "org-static"))))
+            ("org" :components ("org-notes" "org-static"))))
 
-  (org-publish-current-project nil t))
+    (org-publish-current-project)))
 
 (when (eq major-mode 'org-mode)
   (fc--setup-org-mode))

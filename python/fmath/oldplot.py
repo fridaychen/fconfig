b9429fadaad72937952bba8eed3a8e37f494bcb2
friday chen <fridaychen@gmail.com>
2023-06-19 16:23:46 +0900

    refactoring

---
+++ python/fmath/oldplot.py
@@ -0,0 +1,216 @@
+from itertools import chain
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib import rc as prc
+
+ax = None
+axes = []
+
+
+def loadtxt(filename, delimiter=None):
+    with open(filename) as f:
+        return np.loadtxt(
+            f,
+            delimiter=delimiter,
+            dtype="float",
+            comments="#",
+            skiprows=1,
+            usecols=None,
+        )
+
+    return None
+
+
+def get_axes(sub):
+    if type(sub) is tuple:
+        if type(axes[0]) is np.ndarray:
+            return axes[sub[0]][sub[1]]
+        else:
+            return axes[sub[1]]
+    else:
+        return axes[sub]
+
+
+def grid(sub=None, axis="both", caxis="both"):
+    if sub is None:
+        ax.grid(color="gray", linestyle="dashed", axis=axis)
+
+        if caxis:
+            ax.axvline(0, color="#000000")
+            ax.axhline(0, color="#000000")
+    else:
+        subax = get_axes(sub)
+
+        subax.grid(color="gray", linestyle="dashed", axis=axis)
+
+        if caxis == "both" or caxis == "y":
+            subax.axvline(0, color="black")
+
+        if caxis == "both" or caxis == "x":
+            subax.axhline(0, color="black")
+
+
+def setup(
+    height=0,
+    width=0,
+    dpi=0,
+    bg="",
+    title="",
+    font="",
+    xlabel="",
+    ylabel="",
+):
+    if font != "":
+        prc("font", family=font)
+
+    fig = plt.figure()
+
+    if height != 0 and width != 0:
+        fig.set_size_inches(width, height)
+
+    if dpi != 0:
+        fig.set_dpi(dpi)
+
+    if bg != "":
+        fig.set_facecolor(bg)
+
+    global ax
+    ax = plt.axes()
+    if title != "":
+        ax.set_title(title, fontdict={"fontweight": "bold"})
+    ax.set_facecolor(bg)
+
+    if xlabel != "":
+        plt.xlabel(xlabel)
+
+    if ylabel != "":
+        plt.ylabel(ylabel)
+
+
+def bar(x, y, sub=None, mean=True):
+    mean = np.mean(y)
+
+    if sub is None:
+        plt.bar(range(len(y)), y, width=0.6)
+        plt.xticks(range(len(x)), x)
+
+        if mean:
+            ax.axhline(mean, color="#ff0000", linestyle="--")
+    else:
+        subax = get_axes(sub)
+        print("bar subax ", subax)
+
+        subax.bar(range(len(y)), y, width=0.6)
+        subax.set_xticks(range(len(x)), x)
+
+        if mean:
+            subax.axhline(mean, color="#ff0000", linestyle="--")
+
+
+def hist(x, bins=10, type="bar", sub=None):
+    if x is None:
+        return
+
+    if sub is None:
+        plt.hist(x, bins=bins, histtype=type)
+    else:
+        subax = get_axes(sub)
+
+        subax.hist(x, bins=bins, histtype=type)
+
+
+def pie(x, y, sub=None):
+    if sub is None:
+        plt.pie(y, labels=x, shadow=True, autopct="%.0f%%")
+    else:
+        subax = get_axes(sub)
+
+        subax.pie(y, labels=x, shadow=True, autopct="%.0f%%")
+
+
+def plot(x, y, sub=None, label=""):
+    if sub is None:
+        plt.plot(x, y, label=label)
+        if label != "":
+            plt.legend()
+    else:
+        subax = get_axes(sub)
+
+        subax.plot(x, y, label=label)
+        if label != "":
+            subax.legend()
+
+
+def plotf(x, func, sub=None, label=""):
+    if func is None:
+        return
+
+    y = np.array([func(n) for n in x])
+
+    if sub is None:
+        plt.plot(x, y, label=label)
+        if label != "":
+            plt.legend()
+    else:
+        subax = get_axes(sub)
+
+        subax.plot(x, y, label=label)
+        if label != "":
+            subax.legend()
+
+
+def setup_subplot(
+    rows,
+    cols,
+    bg="",
+    height=0,
+    width=0,
+    dpi=0,
+    title="",
+    font="",
+    subtitles=None,
+):
+    global axes
+
+    if font != "":
+        prc("font", family=font)
+
+    fig, axes = plt.subplots(rows, cols)
+
+    if bg != "":
+        fig.set_facecolor(bg)
+
+    if height != 0 and width != 0:
+        fig.set_size_inches(width, height)
+
+    if dpi != 0:
+        fig.set_dpi(dpi)
+
+    if title != "":
+        fig.suptitle(title, fontweight="bold")
+
+    if bg is not None:
+        if type(axes[0]) is np.ndarray:
+            for x in chain.from_iterable(axes):
+                x.set_facecolor(bg)
+        else:
+            for x in axes:
+                x.set_facecolor(bg)
+
+    if subtitles is not None:
+        if type(axes[0]) is np.ndarray:
+            for x, t in zip(
+                list(chain.from_iterable(axes)),
+                list(chain.from_iterable(subtitles)),
+            ):
+                x.set_title(t)
+        else:
+            for x, t in zip(axes, subtitles):
+                x.set_title(t)
+
+
+def save(output):
+    plt.tight_layout()
+    plt.savefig(output, bbox_inches="tight")
--- python/fmath/plot.py
+++ python/fmath/plot.py
@@ -1,12 +1,7 @@
-from itertools import chain
-
 import matplotlib.pyplot as plt
 import numpy as np
 
 from matplotlib import rc as prc
-
-ax = None
-axes = []
 
 
 def loadtxt(filename, delimiter=None):
@@ -23,192 +18,152 @@ def loadtxt(filename, delimiter=None):
     return None
 
 
-def get_axes(sub):
-    if type(sub) is tuple:
-        if type(axes[0]) is np.ndarray:
-            return axes[sub[0]][sub[1]]
+class FigureBase:
+    def __init__(self):
+        pass
+
+    def save(self, fn):
+        self.before_save()
+        self.fig.tight_layout()
+        self.fig.savefig(fn, bbox_inches="tight")
+
+    def before_save(self):
+        pass
+
+    def mark_legend():
+        pass
+
+    def ref(selft):
+        pass
+
+    def grid(self, axis="both", caxis="both"):
+        self.ref().grid(color="gray", linestyle="dashed", axis=axis)
+
+        if caxis:
+            self.ref().axvline(0, color="#000000")
+            self.ref().axhline(0, color="#000000")
+
+    def label(self, xlabel, ylabel):
+        self.ref().set_xlabel(xlabel)
+        self.ref().set_ylabel(ylabel)
+
+    def plot(self, x, y, label=""):
+        self.ref().plot(x, y, label=label)
+        if label != "":
+            self.mark_legend()
+
+    def plotf(self, x, func, label=""):
+        self.plot(x, np.array([func(n) for n in x]), label=label)
+
+    def bar(self, x, y, mean=True):
+        self.ref().bar(range(len(y)), y, width=0.6)
+        self.ref().set_xticks(range(len(x)), x)
+
+        if mean:
+            self.ref().axhline(np.mean(y), color="#ff0000", linestyle="--")
+
+    def hist(self, x, bins=10, type="bar"):
+        self.ref().hist(x, bins=bins, histtype=type)
+
+    def pie(self, x, y):
+        self.ref().pie(y, labels=x, shadow=True, autopct="%.0f%%")
+
+
+class SingleFigure(FigureBase):
+    def __init__(self, bg="", title="", dpi=0, height=0, width=0):
+        super().__init__()
+
+        self.fig, self.ax = plt.subplots()
+        self.enable_legend = False
+
+        if bg != "":
+            self.fig.set_facecolor(bg)
+            self.ax.set_facecolor(bg)
+
+        if title != "":
+            self.ax.set_title(title)
+
+        if dpi != 0:
+            self.fig.set_dpi(dpi)
+
+        if height != 0 and width != 0:
+            print("set figure size")
+            self.fig.set_size_inches(width, height)
+
+    def before_save(self):
+        if self.enable_legend:
+            self.ax.legend()
+
+    def ref(self):
+        return self.ax
+
+    def mark_legend(self):
+        self.enable_legend = True
+
+
+class SubFigure(FigureBase):
+    def __init__(
+        self, sub, bg="", title="", subtitles=None, dpi=0, height=0, width=0
+    ):
+        self.fig, self.axes = plt.subplots(*sub)
+        self.enable_legend = np.full(sub, False)
+        self.f1d = sub[0] == 1
+
+        if bg != "":
+            self.fig.set_facecolor(bg)
+            print(self.axes)
+            for x in self.axes.ravel():
+                x.set_facecolor(bg)
+
+        if title != "":
+            self.fig.suptitle(title)
+
+        if subtitles is not None:
+            for x, t in zip(self.axes.ravel(), subtitles):
+                x.set_title(t)
+
+        if dpi != 0:
+            self.fig.set_dpi(dpi)
+
+        if height != 0 and width != 0:
+            self.fig.set_size_inches(width, height)
+
+    def ref(self):
+        return self.active
+
+    def select(self, sub):
+        if self.f1d:
+            self.active = self.axes[sub[1]]
         else:
-            return axes[sub[1]]
-    else:
-        return axes[sub]
+            self.active = self.axes[*sub]
+        self.active_pos = sub
+
+    def mark_legend(self):
+        self.enable_legend[*self.active_pos] = True
+
+    def before_save(self):
+        for x, b in zip(self.axes.ravel(), self.enable_legend.ravel()):
+            if b:
+                x.legend()
 
 
-def grid(sub=None, axis="both", caxis=True):
-    if sub is None:
-        ax.grid(color="gray", linestyle="dashed", axis=axis)
-
-        if caxis:
-            ax.axvline(0, color="#000000")
-            ax.axhline(0, color="#000000")
-    else:
-        subax = get_axes(sub)
-
-        subax.grid(color="gray", linestyle="dashed", axis=axis)
-
-        if caxis:
-            subax.axvline(0, color="black")
-            subax.axhline(0, color="black")
-
-
-def setup(
-    height=0,
-    width=0,
-    dpi=0,
-    bg="",
-    title="",
-    font="",
-    xlabel="",
-    ylabel="",
+def start_plot(
+    sub="", bg="", font="", dpi=0, title="", subtitles=None, height=0, width=0
 ):
     if font != "":
         prc("font", family=font)
 
-    fig = plt.figure()
-
-    if height != 0 and width != 0:
-        fig.set_size_inches(width, height)
-
-    if dpi != 0:
-        fig.set_dpi(dpi)
-
-    if bg != "":
-        fig.set_facecolor(bg)
-
-    global ax
-    ax = plt.axes()
-    if title != "":
-        ax.set_title(title, fontdict={"fontweight": "bold"})
-    ax.set_facecolor(bg)
-
-    if xlabel != "":
-        plt.xlabel(xlabel)
-
-    if ylabel != "":
-        plt.ylabel(ylabel)
-
-
-def bar(x, y, sub=None, mean=True):
-    mean = np.mean(y)
-
-    if sub is None:
-        plt.bar(range(len(y)), y, width=0.6)
-        plt.xticks(range(len(x)), x)
-
-        if mean:
-            ax.axhline(mean, color="#ff0000", linestyle="--")
+    if sub == "":
+        return SingleFigure(
+            bg=bg, title=title, dpi=dpi, height=height, width=width
+        )
     else:
-        subax = get_axes(sub)
-        print("bar subax ", subax)
-
-        subax.bar(range(len(y)), y, width=0.6)
-        subax.set_xticks(range(len(x)), x)
-
-        if mean:
-            subax.axhline(mean, color="#ff0000", linestyle="--")
-
-
-def hist(x, bins=10, type="bar", sub=None):
-    if x is None:
-        return
-
-    if sub is None:
-        plt.hist(x, bins=bins, histtype=type)
-    else:
-        subax = get_axes(sub)
-
-        subax.hist(x, bins=bins, histtype=type)
-
-
-def pie(x, y, sub=None):
-    if sub is None:
-        plt.pie(y, labels=x, shadow=True, autopct="%.0f%%")
-    else:
-        subax = get_axes(sub)
-
-        subax.pie(y, labels=x, shadow=True, autopct="%.0f%%")
-
-
-def plot(x, y, sub=None, label=""):
-    if sub is None:
-        plt.plot(x, y, label=label)
-        if label != "":
-            plt.legend()
-    else:
-        subax = get_axes(sub)
-
-        subax.plot(x, y, label=label)
-        if label != "":
-            subax.legend()
-
-
-def plotf(x, func, sub=None, label=""):
-    if func is None:
-        return
-
-    y = np.array([func(n) for n in x])
-
-    if sub is None:
-        plt.plot(x, y, label=label)
-        if label != "":
-            plt.legend()
-    else:
-        subax = get_axes(sub)
-
-        subax.plot(x, y, label=label)
-        if label != "":
-            subax.legend()
-
-
-def setup_subplot(
-    rows,
-    cols,
-    bg="",
-    height=0,
-    width=0,
-    dpi=0,
-    title="",
-    font="",
-    subtitles=None,
-):
-    global axes
-
-    if font != "":
-        prc("font", family=font)
-
-    fig, axes = plt.subplots(rows, cols)
-
-    if bg != "":
-        fig.set_facecolor(bg)
-
-    if height != 0 and width != 0:
-        fig.set_size_inches(width, height)
-
-    if dpi != 0:
-        fig.set_dpi(dpi)
-
-    if title != "":
-        fig.suptitle(title, fontweight="bold")
-
-    if bg is not None:
-        if type(axes[0]) is np.ndarray:
-            for x in chain.from_iterable(axes):
-                x.set_facecolor(bg)
-        else:
-            for x in axes:
-                x.set_facecolor(bg)
-
-    if subtitles is not None:
-        if type(axes[0]) is np.ndarray:
-            for x, t in zip(
-                list(chain.from_iterable(axes)),
-                list(chain.from_iterable(subtitles)),
-            ):
-                x.set_title(t)
-        else:
-            for x, t in zip(axes, subtitles):
-                x.set_title(t)
-
-
-def save(output):
-    plt.tight_layout()
-    plt.savefig(output, bbox_inches="tight")
+        return SubFigure(
+            sub,
+            bg=bg,
+            title=title,
+            subtitles=subtitles,
+            dpi=dpi,
+            height=height,
+            width=width,
+        )

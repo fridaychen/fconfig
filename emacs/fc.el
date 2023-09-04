57a4e72c866ce406710108d451656bb1ba3a3b41
friday chen <fridaychen@gmail.com>
2023-09-04 17:04:10 +0900

    bugfix

--- emacs/fc.el
+++ emacs/fc.el
@@ -65,7 +65,7 @@
 (when *is-gui*
   (fc-setup-font))
 
-(fc-add-env-path (expand-file-name "~/.emacs.d/site/python/bin") t)
+(fc-add-env-path (expand-file-name "~/.emacs.d/site/python/bin"))
 
 (fc-add-env-path (concat (getenv "FCHOME") "/python") nil "PYTHONPATH")
 (fc-add-env-path (expand-file-name "~/.local/bin") nil "PATH")

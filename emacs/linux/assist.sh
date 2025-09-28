06fd98794fba18315a017f961aef5fc1ca15adf1
friday chen <fridaychen@gmail.com>
2025-09-28 22:16:32 +0900

    support brightness adjustment through hdmi

--- emacs/linux/assist.sh
+++ emacs/linux/assist.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-function modify_brightness {
-    local delta=$1 current max
+function modify_brightness_through_sysfs {
+    local delta current max
 
-    current=$(</sys/class/backlight/*/brightness)
-    max=$(</sys/class/backlight/*/max_brightness)
+    delta=$(echo "scale=4; $1 / 1000 / 1;" | bc)
+
+    current=$(< /sys/class/backlight/*/brightness)
+    max=$(< /sys/class/backlight/*/max_brightness)
 
     new=$(echo "scale=4; $current/(1.0 * $max)+$delta" | bc)
 
@@ -13,16 +15,34 @@ function modify_brightness {
 
     new_brightness=$(echo "($new * $max + 0.5) / 1" | bc)
 
-    echo $new_brightness >/sys/class/backlight/*/brightness
+    echo $new_brightness > /sys/class/backlight/*/brightness
     echo $(echo "$new*100" | bc)
 }
 
+function modify_brightness_through_hdmi {
+    v=$1
+
+    if [[ $v -gt 0 ]]; then
+        ddcutil setvcp 10 + $v
+    else
+        ddcutil setvcp 10 - $(($v * -1))
+    fi
+}
+
+function modify_brightness {
+    if $(ls /sys/class/backlight/*/brightness 2> /dev/null); then
+        modify_brightness_through_sysfs $*
+    else
+        modify_brightness_through_hdmi $*
+    fi
+}
+
 function dim {
-    modify_brightness -0.005
+    modify_brightness -5
 }
 
 function brighten {
-    modify_brightness 0.005
+    modify_brightness 5
 }
 
 function volume_up {
